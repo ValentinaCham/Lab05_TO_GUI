3d7ea22f452989bbabfb1c199847306ca4d59efb
VALENTINA MILAGROS CHAMBILLA PERCA <90291981+ValentinaCham@users.noreply.github.com>
2023-11-22 13:14:25 -0500

    Manejo de Error Posible

--- ProyectLab05Example/Edit.cpp
+++ ProyectLab05Example/Edit.cpp
@@ -10,7 +10,6 @@ Edit::Edit(QWidget *parent) :
     txtCapital = findChild<QLineEdit*>("txtCapital");
     if (!ui || !txtPais || !txtCapital) {
         qDebug() << "Error: Algunos elementos de la interfaz no fueron encontrados.";
-        // Puedes agregar más manejo de error según sea necesario.
     }
     connect(ui->btnEdit, &QPushButton::clicked, [this]{emit edition(ui->txtPais->text(), ui->txtCapital->text());});
 }
